5f004f5a7373535938dd95d4793528cabd31d93f
M Prasanthi <138520975+Prasanthi-madhan@users.noreply.github.com>
2024-08-01 23:43:11 +0530

    Add files via upload

---
+++ h.js
@@ -0,0 +1,4 @@
+import { Application } from '@splinetool/runtime';
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/78vg2UNXiDJYu4IV/scene.splinecode');
---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multi-text", {
+    strings: ["Data Analytics", "Artificial Intelligence", "Software Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
